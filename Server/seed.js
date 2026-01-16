55261f7144d0747a2f7199c100f48ac4f474647f
Victor Sani <sanivictor38@gmail.com>
2026-01-16 13:04:44 +0100

    fixed seed file

--- Server/seed.js
+++ Server/seed.js
@@ -2,18 +2,49 @@ import { connectDb } from "./db/connectionDb.js"
 import bcrypt from "bcrypt"
 import User from "./models/userModel.js"
 
+// const register = async() => {
+//    try{
+//      await connectDb()
+//     const hashedPassword = await bcrypt.hash("admin", 10)
+//     const user = new User({
+//         name:"admin",
+//         email:"admin@gmail.com",
+//         password:hashedPassword,
+//         address:"address",
+//         role:"admin"
+//     })
+//     await user.save()
+//     console.log('new user created')
+//    }
+//    catch(error){
+// console.error("❌ Seeding failed:", error);
+//     process.exit(1);
+//    }
+// }
+// register() 
+ 
+
 const register = async() => {
-    connectDb()
-    const hashedPassword = await bcrypt.hash("admin", 10)
-    const user = new User({
+   try{
+     console.log("Connecting to DB...");
+     await connectDb()
+     console.log("DB connected, creating user...");
+
+     const hashedPassword = await bcrypt.hash("admin", 10)
+     const user = new User({
         name:"admin",
         email:"admin@gmail.com",
         password:hashedPassword,
         address:"address",
         role:"admin"
-    })
-    await user.save()
-    console.log('new user created')
+     })
+
+     await user.save()
+     console.log('✅ New user created in Atlas')
+   }
+   catch(error){
+     console.error("❌ Seeding failed:", error);
+     process.exit(1);
+   }
 }
 register()
- 
